10884ef4ca513330704192e0b474f256ae5a136c
sungji-kim <tug89459@temple.edu>
2020-04-22 07:03:08 -0700

    added library imports on cache.py

--- index/cache.py
+++ index/cache.py
@@ -4,4 +4,12 @@ def exports():
 
     if 'distdurCache' not in local and 'distdurCache' not in globa:
         distdurCache = {}
-    return distdurCache
+    if 'os' not in local and 'os' not in globa:
+        import os
+    if 'GoogleMaps' not in local and 'GoogleMaps' not in globa:
+        from googlemaps import Client as GoogleMaps
+    if 'pd' not in local and 'pd' not in globa:
+        import pandas as pd
+
+
+    return distdurCache, os, GoogleMaps, pd
